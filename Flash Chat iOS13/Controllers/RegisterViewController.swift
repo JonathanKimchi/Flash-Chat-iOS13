6805d831793f55fad2f2ba2d045503150dbb6ec0
JonathanKimchi <59551654+JonathanKimchi@users.noreply.github.com>
2021-06-12 21:04:16 -0400

    Login, Logout, Register Implemented.

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,29 @@
 //
 
 import UIKit
-
+import Firebase
 class RegisterViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error
+                {
+                    print(e)
+                }
+                else//if there were no errors, execute this statement.
+                {
+                    //Navigate to the Chat View Controller.
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                    //This performs a segue. Since this is performed within a closure,
+                    //All function calls must be prefixed by self if it is in self.
+                }
+                // ...
+            }
+        }
     }
     
 }
